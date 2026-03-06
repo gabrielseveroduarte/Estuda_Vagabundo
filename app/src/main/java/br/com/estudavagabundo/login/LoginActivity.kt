a1aa8a3fc2ae3f42f07380cb429678f711303797
Gabriel Severo Duarte <gabrielseveroduarte15@gmail.com>
2026-03-06 14:17:34 -0300

    Implementaçao do foco no teclado e verificação.

--- app/src/main/java/br/com/estudavagabundo/login/LoginActivity.kt
+++ app/src/main/java/br/com/estudavagabundo/login/LoginActivity.kt
@@ -1,6 +1,15 @@
 package br.com.estudavagabundo.login
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.Patterns
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,14 +26,51 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding.editUsuario.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+            if (hasFocus.not()) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(binding.editUsuario.text.toString())
+                        .matches()
+                ) {
+                    binding.buttonLogin.isEnabled = false
+                    Toast.makeText(this, "Insira um e-mail válido", Toast.LENGTH_SHORT).show()
+                } else {
+                    binding.buttonLogin.isEnabled = binding.editSenha.text.toString().length >= 6
+                }
+            }
         }
+
+        binding.editSenha.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+            if (hasFocus.not()) {
+                if (binding.editSenha.text.toString().length < 6) {
+                    binding.buttonLogin.isEnabled = false
+                    Toast.makeText(
+                        this,
+                        "A senha deve possuir 6 (seis) caracteres ou mais",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    binding.buttonLogin.isEnabled =
+                        Patterns.EMAIL_ADDRESS.matcher(binding.editUsuario.text.toString())
+                            .matches()
+                }
+            }
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            currentFocus.takeIf { it is EditText }?.run {
+                val outRect = Rect()
+                getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    currentFocus?.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(
+                        currentFocus?.windowToken, 0
+                    )
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
