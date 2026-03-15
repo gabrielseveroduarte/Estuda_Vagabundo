5f9178ff9f9ed0f72af4e14c9ac446080df5375b
Gabriel Severo Duarte <gabrielseveroduarte15@gmail.com>
2026-03-15 16:24:50 -0300

    Add Subjects Screen

---
+++ app/src/main/java/br/com/estudavagabundo/subject/AddSubject.kt
@@ -0,0 +1,26 @@
+package br.com.estudavagabundo.subject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.estudavagabundo.R
+
+class AddSubject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_subject)
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
