36a300492ce57a79eeceae6b4bc5d3d321f54809
Gabriel Severo Duarte <gabrielseveroduarte15@gmail.com>
2026-03-15 11:37:09 -0300

    Tela para adicionar materias

---
+++ app/src/main/java/br/com/estudavagabundo/AddSubject.kt
@@ -0,0 +1,25 @@
+package br.com.estudavagabundo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddSubject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_subject)
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
