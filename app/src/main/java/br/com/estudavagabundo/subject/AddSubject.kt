aa06f070d18f36b4efbd8904f551464af4324239
Admin <gabrielseveroduarte15@gmail.com>
2026-04-16 10:01:43 -0300

    Adição de matérias para o RV e também verificação de login.

--- app/src/main/java/br/com/estudavagabundo/subject/AddSubject.kt
+++ app/src/main/java/br/com/estudavagabundo/subject/AddSubject.kt
@@ -13,30 +13,33 @@ class AddSubject : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddSubjectBinding
 
-    private val SubjectsData : MutableList<SubjectsData> = mutableListOf()
+    private val subjectsData: MutableList<SubjectsData> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_subject)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityAddSubjectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val adapter = SubjectAdapter(SubjectsData)
+        val adapter = SubjectAdapter(subjectsData)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
 
-        val nome = binding.edtSubjectName.text.toString().trim()
-        if (nome.isNotEmpty()) {
-            adapter.addSubject(SubjectsData(nome))
-            binding.edtSubjectName.text?.clear()
-        } else {
-            binding.tilSubjectName.error = "Digite o nome da matéria"
+        binding.tilSubjectName.setEndIconOnClickListener {
+            val nome = binding.edtSubjectName.text.toString().trim()
+            if (nome.isNotEmpty()) {
+                adapter.addSubject(SubjectsData(nome))
+                binding.edtSubjectName.text?.clear()
+            } else {
+                binding.tilSubjectName.error = "Digite o nome da matéria"
+            }
         }
-
     }
 }
